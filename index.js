3287fd678db4864ade17afe7f35889054a339462
tejanani26 <bteja2626@gmail.com>
2026-02-19 15:09:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,3 @@ http.createServer(function (req, res) {
   res.write('** Hey my app was deployed :) **'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
-this is my code
